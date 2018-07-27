2e5e9b1f7aab755212755e4cf558d97cd835b0d3
ronaldo.silva <ronaldo.silva@NOC-090059.itamaraty.local>
2018-07-27 04:38:28 -0300

    Primeiro

---
+++ src/java/Dao/LivroDao.java
@@ -0,0 +1,18 @@
+package Dao;
+ 
+import Model.Livro;
+import java.util.List;
+ 
+/**
+*
+* @author José Alexandre
+*/
+public interface LivroDao {
+ 
+public void save(Livro livro);
+public Livro getLivro(long id);
+public List<Livro> list();
+public void remove(Livro livro);
+public void update(Livro livro);
+ 
+}
